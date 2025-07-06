cadbfe6bdafe79b980bb77aabb1fe6e60fdcbcf9
pongpatapee <pongpatapee1021@gmail.com>
2025-07-05 20:11:15 -0400

    feat: create tech read text channel if doesn't exist

--- src/tech_read_bot/bot.py
+++ src/tech_read_bot/bot.py
@@ -7,14 +7,45 @@ from .utils import tabulate_db_objects
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
+intents.guild_messages = True
+
+TEXT_CHANNEL_NAME = "tech_read"
+text_channel_id = None
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 db = TechReadDao()
 
 
+async def create_text_channel(guild):
+    global text_channel_id
+
+    text_channel = discord.utils.get(guild.text_channels, name=TEXT_CHANNEL_NAME)
+    if text_channel:
+        print(
+            f"text channel {TEXT_CHANNEL_NAME} already exists, channel_id = {text_channel.id}"
+        )
+        text_channel_id = text_channel.id
+    else:
+        category = discord.utils.get(guild.categories, name="Text Channels")
+        text_channel = await guild.create_text_channel(
+            TEXT_CHANNEL_NAME, category=category
+        )
+        print(
+            f"created text channel {TEXT_CHANNEL_NAME}, channel_id = {text_channel.id}"
+        )
+        text_channel_id = text_channel.id
+
+    return text_channel
+
+
 @bot.event
 async def on_ready():
     print(f"logged in as {bot.user}")
+
+    for guild in bot.guilds:
+        print(f"- Server: {guild.name} (id: {guild.id})")
+        await create_text_channel(guild)
 
 
 @bot.command(
